af9f45be1b63047abfce627e84dd84d82eda4d61
18130016 <74467162+18130016@users.noreply.github.com>
2021-07-04 09:29:05 +0700

    update
    sad

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes : Routes =[
     component:ContactComponent
   },
   {
-    path:"productdetail",
+    path:"productdetail/:id",
     component:ProductdetailComponent
   },{
   path:"products",
