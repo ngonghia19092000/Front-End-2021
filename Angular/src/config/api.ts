2dc782bacb3e9bd5fde78bead5350a24889bd16d
ngonghia19092000 <73148099+ngonghia19092000@users.noreply.github.com>
2021-07-05 14:40:30 +0700

    update

--- Angular/src/config/api.ts
+++ Angular/src/config/api.ts
@@ -1,5 +1,5 @@
 import {environment} from 'src/environments/environment'
-export const baseUrl = environment.production? 'http://api.shoppingcart.com':'http://localhost:3000'
+export const baseUrl = environment.production? 'http://api.shoppingcart.com':'https://60e2b3879103bd0017b474b0.mockapi.io/api/shopping/'
 export const productUrl=baseUrl+'/products'
 export const cartUrl= baseUrl+'/cart'
 
